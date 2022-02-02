7657215bda572572c962665c9c9ace2018a917e9
Sherwin688 <sherwin.dcosta688@gmail.com>
2022-02-02 22:03:03 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
+
+const bodyParser = require('body-parser')
+const authorRouter = require('./routes/authors')
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -12,6 +16,8 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 app.listen(process.env.PORT || 3000)
 
 const mongoose = require('mongoose')
